232759a59ee100e8bb77029aee0708bfb37c1a81
pratiksaha <pratiksaha89@gmail.com>
2016-10-02 21:58:49 +0200

    basic matplotlib

---
+++ basic_matplotlib.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+
+from scipy.stats import norm
+from pylab import randn
+import matplotlib.pyplot as plt
+import numpy as np
+
+#some data
+x = np.arange(-3, 3, 0.001)
+
+print "line graph"
+plt.plot(x, norm.pdf(x))
+plt.show()
+
+print "multiple plots in a graph"
+plt.plot(x, norm.pdf(x))
+plt.plot(x, norm.pdf(x, 1.0, 0.5))
+plt.show()
+
+print "saving to a file"
+plt.plot(x, norm.pdf(x))
+plt.plot(x, norm.pdf(x, 1.0, 0.5))
+plt.savefig('./graphs/MyPlot.png', format='png')
+
+print "adjusting the axes"
+axes = plt.axes()
+axes.set_xlim([-5, 5])
+axes.set_ylim([0, 1.0])
+axes.set_xticks([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])
+axes.set_yticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+plt.plot(x, norm.pdf(x))
+plt.plot(x, norm.pdf(x, 1.0, 0.5))
+plt.show()
+
+print "adding a grid"
+axes = plt.axes()
+axes.set_xlim([-5, 5])
+axes.set_ylim([0, 1.0])
+axes.set_xticks([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])
+axes.set_yticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+axes.grid()
+plt.plot(x, norm.pdf(x))
+plt.plot(x, norm.pdf(x, 1.0, 0.5))
+plt.show()
+
+print "changing line types and colours"
+axes = plt.axes()
+axes.set_xlim([-5, 5])
+axes.set_ylim([0, 1.0])
+axes.set_xticks([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])
+axes.set_yticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+axes.grid()
+plt.plot(x, norm.pdf(x), 'b-')
+plt.plot(x, norm.pdf(x, 1.0, 0.5), 'r:')
+plt.show()
+
+print "labeling axes and adding legends"
+axes = plt.axes()
+axes.set_xlim([-5, 5])
+axes.set_ylim([0, 1.0])
+axes.set_xticks([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])
+axes.set_yticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+axes.grid()
+plt.xlabel('Greebles')
+plt.ylabel('Probability')
+plt.plot(x, norm.pdf(x), 'b-')
+plt.plot(x, norm.pdf(x, 1.0, 0.5), 'r:')
+plt.legend(['Sneetches', 'Gacks'], loc=4)
+plt.show()
+
+print "xkcd style"
+plt.xkcd()
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+plt.xticks([])
+plt.yticks([])
+ax.set_ylim([-30, 10])
+data = np.ones(100)
+data[70:] -= np.arange(30)
+plt.annotate('THE DAY I REALIZED\nI COULD COOK BACON\nWHENEVER I WANTED', xy=(70, 1), arrowprops=dict(arrowstyle='->'), xytext=(15, -10))
+plt.plot(data)
+plt.xlabel('time')
+plt.ylabel('my overall health')
+plt.show()
+plt.rcdefaults()
+
+print "pie chart"
+values = [12, 55, 4, 32, 14]
+colors = ['r', 'g', 'b', 'c', 'm']
+explode = [0, 0, 0.2, 0, 0]
+labels = ['India', 'United States', 'Russia', 'China', 'Europe']
+plt.pie(values, colors= colors, labels=labels, explode = explode)
+plt.title('Student Locations')
+plt.show()
+
+print "bar chart"
+values = [12, 55, 4, 32, 14]
+colors = ['r', 'g', 'b', 'c', 'm']
+plt.bar(range(0,5), values, color= colors)
+plt.show()
+
+print "scatter plot"
+X = randn(500)
+Y = randn(500)
+plt.scatter(X,Y)
+plt.show()
+
+print "histogram"
+incomes = np.random.normal(27000, 15000, 10000)
+plt.hist(incomes, 50)
+plt.show()
+
+print "box and whisker plot"
+uniformSkewed = np.random.rand(100) * 100 - 40
+high_outliers = np.random.rand(10) * 50 + 100
+low_outliers = np.random.rand(10) * -50 - 100
+data = np.concatenate((uniformSkewed, high_outliers, low_outliers))
+plt.boxplot(data)
+plt.show()
+
+print "age vs tv time"
+ages = np.random.randint(low=0, high=90, size=1000)
+tv = np.random.uniform(low=0, high=10, size=1000)
+plt.xlabel("Age")
+plt.ylabel("TV Time")
+plt.scatter(ages, tv)
+plt.show()
