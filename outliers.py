0983d45c92c5b173067b94c3f15fb02522e768d1
pratiksaha <pratiksaha89@gmail.com>
2016-10-09 22:25:43 +0200

    outliers

--- outliers.py
+++ outliers.py
@@ -9,24 +9,23 @@ def reject_outliers(data):
     filtered = [e for e in data if (u - 2 * s < e < u + 2 * s)]
     return filtered
 
-#random data
+print "\nSome Random Data:"
 incomes = np.random.normal(27000, 15000, 10000)
-print incomes.mean()
-print incomes.std()
+print "Mean :", incomes.mean()
+print "Std Dev :", incomes.std()
 plt.hist(incomes,50)
 plt.show()
 
-#add some outlier
+print "\nAdded Some Outlier:"
 incomes = np.append(incomes, [1000000000])
-print incomes.mean()
-print incomes.std()
+print "Mean :", incomes.mean()
+print "Std Dev :", incomes.std()
 plt.hist(incomes,50)
 plt.show()
 
-#filter outliers
-filtered = reject_outliers(incomes)
-print np.mean(filtered)
-print np.std(filtered)
-plt.hist(filtered, 50)
+print "\nFiltered Data:"
+incomes = reject_outliers(incomes)
+print "Mean :", np.mean(incomes)
+print "Std Dev :", np.std(incomes)
+plt.hist(incomes, 50)
 plt.show()
-
