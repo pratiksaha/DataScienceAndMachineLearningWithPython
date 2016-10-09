084a083946b5a73343a784451eb55829e5406f87
pratiksaha <pratiksaha89@gmail.com>
2016-10-09 21:34:09 +0200

    k fold crossvalidation

--- iris_analysis.py
+++ iris_analysis.py
@@ -4,6 +4,8 @@ import itertools
 import matplotlib.pyplot
 import sklearn.datasets
 import sklearn.decomposition
+import sklearn.svm
+import sklearn.cross_validation
 
 print "Iris Data..."
 iris = sklearn.datasets.load_iris()
@@ -46,3 +48,33 @@ print "\nInformation (variance) preserved :"
 print pca_1d.explained_variance_ratio_
 print "\nTotal Variance preserved :"
 print sum(pca_1d.explained_variance_ratio_)
+
+print "\nK fold cross validation with 60% training and 40% testing :\n"
+X_train, X_test, y_train, y_test = sklearn.cross_validation.train_test_split(iris.data, iris.target, test_size=0.4, random_state=0)
+
+print "Running SVC with default(i.e degree=3) poly kernel on training data"
+clf = sklearn.svm.SVC(kernel='poly').fit(X_train, y_train)
+print "Score with training data:", clf.score(X_train, y_train)
+print "Score with test data:", clf.score(X_test, y_test)
+print "Using cross validation Scores :"
+scores = sklearn.cross_validation.cross_val_score(clf, iris.data, iris.target, cv=5)
+print scores
+print "Mean Score : %s \n" %(scores.mean())
+
+print "Running SVC with poly kernel of degree 2 on training data"
+clf = sklearn.svm.SVC(kernel='poly', degree=2).fit(X_train, y_train)
+print "Score with training data:", clf.score(X_train, y_train)
+print "Score with test data:", clf.score(X_test, y_test)
+print "Using cross validation Scores :"
+scores = sklearn.cross_validation.cross_val_score(clf, iris.data, iris.target, cv=5)
+print scores
+print "Mean Score : %s \n" %(scores.mean())
+
+print "Running SVC with linear kernel on training data"
+clf = sklearn.svm.SVC(kernel='linear').fit(X_train, y_train)
+print "Score with training data:", clf.score(X_train, y_train)
+print "Score with test data:", clf.score(X_test, y_test)
+print "Using cross validation Scores :"
+scores = sklearn.cross_validation.cross_val_score(clf, iris.data, iris.target, cv=5)
+print scores
+print "Mean Score : %s \n" %(scores.mean())
