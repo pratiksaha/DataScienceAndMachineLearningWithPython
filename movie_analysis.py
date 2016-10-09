cc541b40165c8902fbb25cba8c261078dc462757
pratiksaha <pratiksaha89@gmail.com>
2016-10-09 21:12:44 +0200

    move some file around

--- movie_analysis.py
+++ movie_analysis.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python
 
+import operator
 import pandas
 import numpy
+import scipy.spatial
+
+def ComputeDistance(a, b):
+    genresA = a[1]
+    genresB = b[1]
+    genreDistance = scipy.spatial.distance.cosine(genresA, genresB)
+    popularityA = a[2]
+    popularityB = b[2]
+    popularityDistance = abs(popularityA - popularityB)
+    return genreDistance + popularityDistance
+
+def getNeighbors(movieID, K):
+    distances = []
+    for movie in movieDict:
+        if (movie != movieID):
+            dist = ComputeDistance(movieDict[movieID], movieDict[movie])
+            distances.append((movie, dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors = []
+    for x in range(K):
+        neighbors.append(distances[x][0])
+    return neighbors
 
 print "\nRatings:"
 r_cols = ['user_id', 'movie_id', 'rating']
@@ -89,3 +112,37 @@ print simCandidates.head(10)
 print "\nRemoving Movies I have already watched & rated..."
 filteredSims = simCandidates.drop(myRatings.index)
 print filteredSims.head(10)
+
+print "\nGrouping by total and average ratings :"
+movieProperties = ratings.groupby('movie_id').agg({'rating': [numpy.size, numpy.mean]})
+print movieProperties.head()
+
+print "\nNormalising..."
+movieNumRatings = pandas.DataFrame(movieProperties['rating']['size'])
+movieNormalizedNumRatings = movieNumRatings.apply(lambda x: (x - numpy.min(x)) / (numpy.max(x) - numpy.min(x)))
+print movieNormalizedNumRatings.head()
+
+print "\nGetting genre info.."
+movieDict = {}
+with open(r'./data/movielens/u.item') as f:
+    temp = ''
+    for line in f:
+        fields = line.rstrip('\n').split('|')
+        movieID = int(fields[0]) 
+        name = fields[1]
+        genres = fields[5:25]
+        genres = map(int, genres)
+        movieDict[movieID] = (name, genres, movieNormalizedNumRatings.loc[movieID].get('size'), movieProperties.loc[movieID].rating.get('mean'))
+print "Sample Data ", movieDict[1]
+print "Sample Data ", movieDict[2]
+print "Sample Data ", movieDict[3]
+print "Sample Data ", movieDict[4]
+print "\nDistance b/w movie %s & %s is %s\n" %(movieDict[2][0], movieDict[4][0], ComputeDistance(movieDict[2], movieDict[4]))
+K = 10
+avgRating = 0
+neighbors = getNeighbors(1, K)
+for neighbor in neighbors:
+    avgRating += movieDict[neighbor][3]
+    print movieDict[neighbor][0] + " " + str(movieDict[neighbor][3])
+    avgRating /= float(K)
+print "\nAvg rating for %s nearest neighbours to %s is %s\n" %(K, movieDict[1][0], avgRating)
